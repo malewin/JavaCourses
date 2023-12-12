56b608ed874ee155f05ad56993b0e2f7bd6b4fbe
malewin <139165145+malewin@users.noreply.github.com>
2023-12-12 23:17:27 +0900

    task is resolved

---
+++ seminars/seminar3ArrayListSort.java
@@ -0,0 +1,25 @@
+package seminars;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+
+public class seminar3ArrayListSort {
+
+    public static void main(String[] args) {
+        List <Integer> list = new ArrayList<>();
+        Random random = new Random();
+        int n = 10;
+
+        for (int i = 0; i < n; i++) {
+            list.add(random.nextInt(10)); 
+        }
+        System.out.println(list);
+        list.sort(null); // от меньшего к большему
+        System.out.println(list);
+        list.sort(Comparator.reverseOrder()); // от большего к меньшему
+        System.out.println(list);
+        
+    }
+}
